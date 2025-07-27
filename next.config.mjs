ad8b5c27c9468244f5612671b16a3a6ffae9eea3
ramez <ramezgamel52@gmail.com>
2025-07-27 17:14:01 +0300

    modify next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: { unoptimized: true },
-};
-
-export default withSentryConfig(
-  nextConfig,
-  {
-    silent: true,
-    org: "javascript-nextjs",
-    project: "javascript-nextjs",
-  },
-  {
-    widenClientFileUpload: true,
-
-    transpileClientSDK: true,
-
-    hideSourceMaps: true,
-
-    disableLogger: true,
-
-    automaticVercelMonitors: true,
-  }
-);
+const nextConfig = {};
+export default nextConfig;
